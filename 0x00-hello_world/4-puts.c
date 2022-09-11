f57c5afb69d5816d2bd5fe6518215eaffaaf2f36
David Anderson <davidans2019@gmail.com>
2022-09-11 09:38:03 -0700

    added 4-puts working but sure if correct

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - entry point
+ *
+ * returns: always returns  (success)
+ *
+ *print srtring to screen
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
