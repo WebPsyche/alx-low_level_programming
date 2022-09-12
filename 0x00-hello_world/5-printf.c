37da1c729551597317a2b56ea5fd1f49792c6aa4
David Anderson <davidans2019@gmail.com>
2022-09-12 14:42:14 -0700

    corrected 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
+ * main - entry point Description: prints to the screen
+ *
  * printf - prints to the screen
  *
  * returns 0
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
