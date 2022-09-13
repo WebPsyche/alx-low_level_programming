5978c4202e77c24d47d4b6ac8b5bd95c5f9e3dd3
David Anderson <davidans2019@gmail.com>
2022-09-13 15:51:14 -0700

    2-print

---
+++ 0x02-functions_nested_loops/2-print_alphabets_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_X10 - print the alphabet 10 times in lwc
+ *
+ * Return : Always return 0
+ */
+void print_alphabet_x10(void)
+{
+	char alpha10;
+	int i;
+	i = 0;
+	while (i < 10)
+	{
+		alpha10 = 'a
+		while (alpha10 <= 'z')
+		{
+			_putchar(alpha10);
+			alpha10++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
