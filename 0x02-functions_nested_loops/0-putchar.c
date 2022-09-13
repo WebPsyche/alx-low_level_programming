87d5c11a9261691ae0793fed88f258097bc2254c
David Anderson <davidans2019@gmail.com>
2022-09-13 12:34:39 -0700

    added 0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/*
+ * putchar file
+ *
+ *Description: write a program that prints _putchar followed by a new line.
+ *
+ * use putchar function
+ *
+ * Return type : Always 0 succes
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ * main - entry point
+ *
+ * get putchar function and passin parameter
+ *
+ * Retun type: return int type
+ */
+int putchar(int puts);
+int main(void)
+
+{
+	int putch;
+	putch = putchar("_putchar");
+	return (putch);
+}
+
+
