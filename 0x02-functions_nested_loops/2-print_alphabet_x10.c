c9d2274a5f9316e8e930dec606e043c22c7fb20c
David Anderson <davidans2019@gmail.com>
2022-09-14 02:41:47 -0700

    corrected 2-alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * print_alphabet_X10 - print the alphabet 10 times in lwc
- *
- * Return : Always return 0
+ *print_alphabet_x10 - print the 10 times the alphabet, in lwc
+ *Return : Always return 0
  */
 void print_alphabet_x10(void)
 {
 	char alpha10;
 	int i;
+
 	i = 0;
 	while (i < 10)
 	{
