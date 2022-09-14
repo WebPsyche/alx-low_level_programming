498c97dffbabd3d1fd1af48d14c91a26357dcd7f
David Anderson <davidans2019@gmail.com>
2022-09-14 05:22:37 -0700

    added 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - check for lowercase letters
+ *
+ * @c: the character to be checked
+ *
+ * Return: 1 if c is lowercase, 0 if otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = _islower('H');
+	_putchar(r + '0');
+	return (0);
+}
