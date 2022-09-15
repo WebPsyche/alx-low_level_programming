57487697c40414592ef3fb7690ce94b31d166722
David Anderson <davidans2019@gmail.com>
2022-09-15 10:12:03 -0700

    added 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,4 +6,17 @@
  */
 void print_most_numbers(void)
 {
-	
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if ((i == 2) || (i == 4))
+		{
+		}
+		else
+		{
+			_putchar('0' + i);
+		}
+	}
+	_putchar('0' + i);
+}
