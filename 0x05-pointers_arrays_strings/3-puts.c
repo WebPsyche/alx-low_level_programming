4681ee0dd52aa4f76da56f29e98cc32178f44f0d
David Anderson <davidans2019@gmail.com>
2022-09-20 07:25:17 -0700

    added 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _puts - print a string followed by a new line to
+ * standard output
+ *
+ * @str: string to be printed
+ *
+ * Return: return string with new line
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
