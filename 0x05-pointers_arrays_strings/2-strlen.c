0cc2bd25a447c918b22a483463f278c6ecdd79c1
David Anderson <davidans2019@gmail.com>
2022-09-20 06:25:34 -0700

    corr 2x3

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * _strien - returns the length of a stringg
+ * _strlen - returns the length of a stringg
  *
  * @s: length of string to be rerturned
  *
  * Return: returns the length of string
  */
-int _strien(char *s)
+int _strlen(char *s)
 {
 	int i;
 
