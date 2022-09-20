4f95b95f3a642c7aa6c0f26efdefd98c741559ce
David Anderson <davidans2019@gmail.com>
2022-09-20 06:09:46 -0700

    added 2-strien.c

---
+++ 0x05-pointers_arrays_strings/2-strien.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _strien - returns the length of a stringg
+ *
+ * @s: length of string to be rerturned
+ *
+ * Return: returns the length of string
+ */
+int _strien(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+	}
+	return (i);
+}
