87bdd9fb1af307e97be529109612347ee61570f5
David Anderson <davidans2019@gmail.com>
2022-09-20 12:45:30 -0700

    cor 7x5

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -20,7 +20,7 @@ void puts_half(char *str)
 	}
 	if (i % 2 == 0)
 	{
-		x = i/2;
+		x = i / 2;
 		while (x < i)
 		{
 			_putchar(str[x]);
@@ -29,7 +29,7 @@ void puts_half(char *str)
 	}
 	else if (i % 2 != 0)
 	{
-		x = (i - 1)/2;
+		x = i - 1 / 2;
 		while (x < i)
 		{
 			_putchar(str[x]);
