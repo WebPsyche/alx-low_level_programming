a9108183fa1021b3e8177ea4f8e9a6dd536de3c9
David Anderson <davidans2019@gmail.com>
2022-09-21 09:01:55 -0700

    added 0-strcat.c README.md

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcat - contatenates two strings
+ *
+ * @dest: string to be overwritten
+ * @src: string to be appended
+ *
+ * Return: a ponter to the reusulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, x;
+
+	x = 0;
+	for (i  = 0; dest[i] != '\0'; i++)
+	{
+		i++;
+	}
+	while( src[x] != '\0')
+	{
+		dest[i] = src[x];
+		x++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+	
