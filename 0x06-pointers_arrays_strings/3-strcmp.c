5cc5b48e69ba249e41a8cbc8200d23acfe9cfb2d
David Anderson <davidans2019@gmail.com>
2022-09-21 12:43:43 -0700

    asdfg

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,7 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, x;
-
-	i = 0;
-	x = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	for (int i = 0; (s1[i] != '\0' && s2[i] != '\0'); i++)
 	{
 		if (s1[i] > s2[i])
 		{
